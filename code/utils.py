7821e97aed4aed187dcae62bc8bfab75da770fc5
David Branner <dpb@dpb15.local>
2014-10-25 10:54:35 -0400

    add utils with two initial functions

---
+++ code/utils.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# utils.py
+# David Prager Branner
+# 20141025
+
+import string
+import difflib as D
+
+all_punct = string.punctuation + (
+        """　﹉﹊﹋﹌＿﹍﹎﹏︳︴－﹣︲﹘︱〜･，﹐︐、﹑︑､﹅﹆；﹔︔：﹕︓！﹗︕？﹖︖．﹒‥︰︙。︒｡＇＂〝〞〟（﹙︵）﹚︶［﹇］﹈｛﹛︷｝﹜︸｟｠〈︿〉﹀《︽》︾「﹁｢」﹂｣『﹃』﹄【︻】︼〔﹝︹〕﹞︺〖︗〗︘〘〙〚〛﹫＊﹡／＼﹨＆﹠＃﹟％﹪〃｀＾＋﹢＜﹤＝﹦>＞﹥￢｜￤～￨￮〄〒〓々＄﹩〇〆﹓﹧＀""")
+
+def clean_for_edit_distance(s):
+    """Convert to list and strip punctuation; if Chinese, use special rule."""
+    cleaned = [i.strip(string.punctuation) for i in s.split()]
+    # Deal with Chinese case.
+    if [s] == cleaned:
+        cleaned = [i.strip(all_punct) for i in list(s)]
+        cleaned = [i for i in cleaned if i]
+    return cleaned
+
+def get_edit_distance(s1, s2):
+    """Clean strings and return difflib.Sequencematcher ratio()."""
+    sequence = D.SequenceMatcher(None,
+            clean_for_edit_distance(s1),
+            clean_for_edit_distance(s2))
+    return sequence.ratio()
